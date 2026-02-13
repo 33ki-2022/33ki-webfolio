a979f914e2de1eda637e5930c198b0d67b248ead
33ki-2022 <33ki.2022@gmail.com>
2026-02-13 23:03:53 +0900

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://username.github.io',
-  base: '/repository-name/',
+  base: '/33ki-webfolio/',
 });
